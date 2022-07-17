8e07abd32361343bb19e0d0ef370b2fe466bf887
Olga Mazyuk <olgamazyuk01@gmail.com>
2022-07-17 20:05:24 +0300

    init: create basic structure

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
+import { readFile } from 'fs/promises';
+import { dirname, join } from 'path';
+import { parse } from 'yaml';
+import { SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const rootDirname = dirname(__dirname);
+  const DOC_API = await readFile(join(rootDirname, 'doc', 'api.yaml'), 'utf-8');
+  const document = parse(DOC_API);
+
+  SwaggerModule.setup('doc', app, document);
+
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+
   await app.listen(4000);
 }
 bootstrap();
