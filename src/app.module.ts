58b7d767fca01f9da615e63ec7affd6e70986054
Olga Mazyuk <olgamazyuk01@gmail.com>
2022-08-06 19:29:35 +0300

    feat: add config to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
@@ -18,6 +19,9 @@ import { AuthModule } from './auth/auth.module';
     FavoritesModule,
     PrismaModule,
     AuthModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
