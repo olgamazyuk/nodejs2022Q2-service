8fb43f3f8333beb47617c867c783ec0472352c25
Olga Mazyuk <olgamazyuk01@gmail.com>
2022-08-07 19:13:39 +0300

    feat: change main.ts

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,12 @@ import { SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import 'dotenv/config';
+import { HttpExceptionFilter } from './logging/exception.filter';
+import { LoggingService } from './logging/logging.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalFilters(new HttpExceptionFilter());
 
   const rootDirname = dirname(__dirname);
   const DOC_API = await readFile(join(rootDirname, 'doc', 'api.yaml'), 'utf-8');
@@ -17,6 +20,18 @@ async function bootstrap() {
   SwaggerModule.setup('doc', app, document);
 
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+
+  const logger = new LoggingService();
+
+  process.on('uncaughtException', (error) => {
+    logger.error(`Uncaught Exception --- ${error}`);
+    process.exit(1);
+  });
+
+  process.on('unhandledRejection', (reason) => {
+    logger.error(`Unhandled Rejection --- ${reason}`);
+    process.exit(1);
+  });
 
   await app.listen(process.env.PORT_SERVICE || 4000);
 }
